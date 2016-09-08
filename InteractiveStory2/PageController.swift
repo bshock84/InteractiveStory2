c86167c3137c47a94cc2644ddf09f51dddb9f852
bshock84 <bshock84@yahoo.com>
2016-09-07 22:49:17 -0500

    implemented the view controller and page controller classes

---
+++ InteractiveStory2/PageController.swift
@@ -0,0 +1,156 @@
+//
+//  PageController.swift
+//  InteractiveStory2
+//
+//  Created by Ben Shockley on 9/5/16.
+//  Copyright © 2016 Ben Shockley. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+
+    var page: Page?
+    
+    let artwork = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        //assigning a locally scoped constant, page, to the globally scoped constant page, and checking that the assignment works.  if so it prints the text in the story.
+        if let page = page {
+            artwork.image = page.story.artwork
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+            let paragraphStyle = NSMutableParagraphStyle()
+            
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            storyLabel.attributedText = attributedString
+            
+            
+            if let firstChoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.loadFirstChoice), for: .touchUpInside)
+            } else {
+                firstChoiceButton.setTitle("Play Again?", for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.playAgain), for: .touchUpInside)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+                secondChoiceButton.addTarget(self, action: #selector(PageController.loadSecondChoice), for: .touchUpInside)
+            }
+            
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillLayoutSubviews() {
+        view.addSubview(artwork)
+        artwork.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            artwork.topAnchor.constraint(equalTo: view.topAnchor),
+            artwork.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            artwork.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artwork.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+        
+        view.addSubview(storyLabel)
+        storyLabel.numberOfLines = 0
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 10.0)
+            ])
+        
+        view.addSubview(firstChoiceButton)
+        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
+        
+        view.addSubview(secondChoiceButton)
+        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+            ])
+    }
+    
+    func loadFirstChoice() {
+        if let page = page, let firstChoice = page.firstChoice {
+            let nextPage = firstChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    func loadSecondChoice() {
+        if let page = page, let secondChoice = page.secondChoice {
+            let nextPage = secondChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    
+    func playAgain() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- InteractiveStory2/ViewController.swift
+++ InteractiveStory2/ViewController.swift
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //If it is the correct segue, identified by the string that we decided in the storyboard.
+        // then we assign a constant, pageController, to the destination ViewController, and cast it as a PageController.
+        // Then we assign the page property of pageController to the Adventure struct, and cause story to return to start of our story.
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destination as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
+    }
 
 }
 
